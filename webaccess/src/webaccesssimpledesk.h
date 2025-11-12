9c1edaaf46ab45544d4e08f23e36ab2feeb4fb74
jpue <94829332+jpue@users.noreply.github.com>
2025-11-12 16:05:24 +0100

    webaccess: integrate DMX Keypad into WebAccess Simple Desk C++ code

--- webaccess/src/webaccesssimpledesk.h
+++ webaccess/src/webaccesssimpledesk.h
@@ -34,6 +34,7 @@ public:
     static QString getHTML(Doc *doc, SimpleDesk *sd);
     static QString getChannelsMessage(Doc *doc, SimpleDesk *sd,
                                       quint32 universe, int startAddr, int chNumber);
+    static QString getKeypadHTML(Doc *doc, SimpleDesk *sd);
 
 signals:
 
